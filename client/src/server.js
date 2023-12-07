e00fda5ec53c61ece04f0a61ebee53c7b0845c98
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-12-07 16:56:37 +0100

    Task Completed

--- client/src/server.js
+++ client/src/server.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const server = axios.create({
-  baseURL: "http://localhost:3042",
+  //baseURL: "http://localhost:3042",
+  baseURL: "https://ecdsa-node-fawn.vercel.app",
 });
 
 export default server;
